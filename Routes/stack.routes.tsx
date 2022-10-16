04ba476dafb020193107e0416fc0aaf313d70eed
santosfk <santosguilhermefk@gmail.com>
2022-10-15 23:24:24 -0300

    feat: first commit

---
+++ Routes/stack.routes.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const { Screen, Navigator } = createNativeStackNavigator();
+export const CheckinRoutes = () => {
+  return <div>stack.routes</div>;
+};
