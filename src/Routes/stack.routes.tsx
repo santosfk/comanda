e0f5702e67d5aa0bdcd48bfc73b03dfdddfac41c
santosfk <santosguilhermefk@gmail.com>
2022-10-23 22:52:59 -0300

    feat: add and config styled-components

--- src/Routes/stack.routes.tsx
+++ src/Routes/stack.routes.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Checkin } from "../Screens/Checkin";
-import { ClientScreen } from "../Screens/ClientScreen";
-import { WaiterScreen } from "../Screens/WaiterScreen";
+import { Home } from "../Screens/Home";
 const { Screen, Navigator } = createNativeStackNavigator();
 export const CheckinRoutes = () => {
   return (
     <Navigator>
       <Screen name="checkin" component={Checkin} />
-      <Screen name="client" component={ClientScreen} />
-      <Screen name="waiter" component={WaiterScreen} />
+      <Screen name="home" component={Home} />
     </Navigator>
   );
 };
