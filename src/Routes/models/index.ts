987339287308b544a655cc5c1131a85c81960fc7
santosfk <santosguilhermefk@gmail.com>
2022-10-23 22:53:17 -0300

    fix: routes types

---
+++ src/Routes/models/index.ts
@@ -0,0 +1,6 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+type RootParamsList = {
+  checkin: undefined;
+  home: undefined;
+};
+export type propsStack = NativeStackNavigationProp<RootParamsList>;
