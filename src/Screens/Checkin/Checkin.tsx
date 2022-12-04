8bc81d430835dac1302f5f25f0f572244d11c7a1
santosfk <santosguilhermefk@gmail.com>
2022-12-03 21:29:05 -0300

    feat: create checkin page

--- src/Screens/Checkin/Checkin.tsx
+++ src/Screens/Checkin/Checkin.tsx
@@ -1,6 +1,43 @@
 import React from "react";
-import { View } from "react-native";
+import {
+  Container,
+  EnterButton,
+  ButtonText,
+  ButtonWrapper,
+  ExitButton,
+  LogoText,
+  LogoWrapper
+} from "./style";
+import { useNavigation } from "@react-navigation/native";
+import { Text,Image } from "react-native";
+import { propsStack } from "../../Routes/models";
+import  MaterialCommunityIcons  from '@expo/vector-icons/MaterialCommunityIcons';
 
 export const Checkin = () => {
-  return <View></View>;
+  const navigation = useNavigation<propsStack>();
+  const goToHome = () => {
+    navigation.navigate("home");
+  };
+  return (
+    <Container>
+      <LogoWrapper>
+      <Image
+        
+        source={require('../../assets/comanda.png')}
+      />
+        <LogoText>
+        ANOTE SEUS PEDIDOS SEM ERRO!!
+        </LogoText>
+      </LogoWrapper>
+      <ButtonWrapper>
+        <EnterButton onPress={goToHome}>
+          <ButtonText>Entrar</ButtonText>
+        </EnterButton>
+        <ExitButton onPress={goToHome}>
+          <ButtonText>Sair</ButtonText>
+        </ExitButton>
+      </ButtonWrapper>
+      <MaterialCommunityIcons name='hamburger' size={50} color='black'/>
+    </Container>
+  );
 };
