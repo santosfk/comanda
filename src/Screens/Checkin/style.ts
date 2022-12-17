faff14da6c1700fdad54150c012c1849e5dd0c9f
santosfk <santosguilhermefk@gmail.com>
2022-12-17 13:36:48 -0300

    feat: add tbemes in components

--- src/Screens/Checkin/style.ts
+++ src/Screens/Checkin/style.ts
@@ -1,38 +1,39 @@
 import styled from "styled-components/native";
- export const Container = styled.View`
-justify-content: space-around;
-align-items: center;
-flex: 1;
- `
- export const EnterButton  = styled.Pressable`
- background-color: #2EDBBC;
-align-items: center;
-justify-content: center;
-padding: 20px 100px;
-border-radius: 10;
- `
- export const ExitButton = styled.Pressable`
-background-color: black;
-align-items: center;
-justify-content: center;
-padding: 30px 100px;
-border-radius: 15;
- `
- export const ButtonText = styled.Text`
- font-weight: bold;
- color: white;
- font-size: 28;
- `
- export const ButtonWrapper = styled.View`
- height: 300px;
- flex-direction: column;
- align-items: center;
-justify-content: space-evenly;
- `
- export const LogoWrapper = styled.View`
-justify-content: center;
-align-items: center;
- `
- export const LogoText =styled.Text`
- color: #26A58E;
- `
+export const Container = styled.View`
+  justify-content: space-around;
+  align-items: center;
+  flex: 1;
+  background-color: ${({ theme }) => theme.primary_background};
+`;
+export const EnterButton = styled.Pressable`
+  background-color: #2edbbc;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 100px;
+  border-radius: 10;
+`;
+export const ExitButton = styled.Pressable`
+  background-color: black;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 100px;
+  border-radius: 15;
+`;
+export const ButtonText = styled.Text`
+  font-weight: bold;
+  color: white;
+  font-size: 28;
+`;
+export const ButtonWrapper = styled.View`
+  height: 300px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+export const LogoWrapper = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+export const LogoText = styled.Text`
+  color: #26a58e;
+`;
