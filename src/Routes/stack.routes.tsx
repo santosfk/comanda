7bef6c503b8aba84cf27c137bac4f068c2675d0d
santosfk <santosguilhermefk@gmail.com>
2022-12-18 21:16:48 -0300

    feat: add getName screen

--- src/Routes/stack.routes.tsx
+++ src/Routes/stack.routes.tsx
@@ -3,17 +3,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Checkin } from "../Screens/Checkin";
 import { HomeRoutes } from "./HomeRoutes";
+import { GetName } from "../Screens/GetName";
 const { Screen, Navigator } = createNativeStackNavigator();
 export const CheckinRoutes = () => {
   return (
     <Navigator
-    screenOptions={{
-      headerShown: true,
-      headerTintColor:'#26A58E'
-     
-    }}
+      screenOptions={{
+        headerShown: true,
+        headerTintColor: "#26A58E",
+      }}
     >
       <Screen name="checkin" component={Checkin} />
+      <Screen name="getName" component={GetName} />
       <Screen name="home" component={HomeRoutes} />
     </Navigator>
   );
