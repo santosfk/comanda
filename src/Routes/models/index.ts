9a3ae5b32b66022c30d85f10153ff6c268ad60cb
santosfk <santosguilhermefk@gmail.com>
2022-12-18 21:17:07 -0300

    feat: add new route in types

--- src/Routes/models/index.ts
+++ src/Routes/models/index.ts
@@ -2,5 +2,6 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 type RootParamsList = {
   checkin: undefined;
   home: undefined;
+  getName:undefined
 };
 export type propsStack = NativeStackNavigationProp<RootParamsList>;
