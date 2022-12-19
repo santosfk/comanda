886dffbd39b0bfcd55e7fb8a2d126daecb00bc46
santosfk <santosguilhermefk@gmail.com>
2022-12-18 21:16:29 -0300

    feat: bottom navigator in home screen

--- src/Routes/HomeRoutes.tsx
+++ src/Routes/HomeRoutes.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Checkin } from "../Screens/Checkin";
 import { Home } from "../Screens/Home";
 
 const Tab = createBottomTabNavigator();
 export const HomeRoutes = () => {
   return (
-
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-      </Tab.Navigator>
-
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
   );
 };
