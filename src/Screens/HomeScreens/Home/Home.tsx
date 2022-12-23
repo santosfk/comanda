fa5738ba414f72bba9b6af977669a0a6f8b9b76f
santosfk <santosguilhermefk@gmail.com>
2022-12-22 23:27:47 -0300

    feat: creaate home tab

---
+++ src/Screens/HomeScreens/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import { View, Text } from "react-native";
+import { Container } from "./style";
+
+export const Home = () => {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  );
+};
