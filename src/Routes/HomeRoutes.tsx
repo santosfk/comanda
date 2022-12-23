77e49ef226752f06c995b6aa79349f851f35a028
santosfk <santosguilhermefk@gmail.com>
2022-12-22 23:26:00 -0300

    fix import of the home

--- src/Routes/HomeRoutes.tsx
+++ src/Routes/HomeRoutes.tsx
@@ -2,13 +2,36 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Checkin } from "../Screens/Checkin";
-import { Home } from "../Screens/Home";
-
+import { Home } from "../Screens/HomeScreens/Home";
+import { AntDesign, Entypo, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Search } from "../Screens/HomeScreens/Search";
 const Tab = createBottomTabNavigator();
 export const HomeRoutes = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, size }) => {
+        let iconName;
+        let color = focused? "#2EDBBC" : 'black'
+        if (route.name === 'home') { 
+          return <Entypo name="home"  size={size} color={color}/> 
+         }
+         if(route.name === 'search'){
+          return <FontAwesome name="search" size={size} color={color}/>
+         }
+         
+    
+
+        // You can return any component that you like here!
+       
+      
+      },
+      tabBarActiveTintColor: '#2EDBBC',
+      tabBarInactiveTintColor: 'gray',
+    })}
+    >
+      <Tab.Screen name="home" component={Home}  />
+      <Tab.Screen name="search" component={Search}  />
     </Tab.Navigator>
   );
 };
