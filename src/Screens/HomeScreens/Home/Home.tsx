d3714294db9280aa535a05a484e3a0ed3dda9a8f
santosfk <santosguilhermefk@gmail.com>
2022-12-26 23:08:11 -0300

    add list of tables in home screen

--- src/Screens/HomeScreens/Home/Home.tsx
+++ src/Screens/HomeScreens/Home/Home.tsx
@@ -1,11 +1,22 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { Container } from "./style";
-
+import table from "../../../mocks/table.json";
+import { Table } from "../../../components/Table";
 export const Home = () => {
   return (
     <Container>
-      <Text>Home</Text>
+      <FlatList
+        data={table.tables}
+        renderItem={({ item }) => (
+          <Table
+            key={item.id}
+            id={item.id}
+            clients={item.clients_number}
+            status={item.status}
+          />
+        )}
+      />
     </Container>
   );
 };
