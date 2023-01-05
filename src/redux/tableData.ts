db293037d21faa86943cfbd9f33a67f89603c4c3
santosfk <santosguilhermefk@gmail.com>
2023-01-05 20:03:31 -0300

    feat: create tableData Slice

---
+++ src/redux/tableData.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const infoTableModel = {
+  id: undefined,
+  status: "",
+  clients_number: undefined,
+  products: [
+    {
+      product: {
+        name: "",
+        price: undefined,
+      },
+    },
+  ],
+};
+export const slice = createSlice({
+  name: "tableInfo",
+  initialState: infoTableModel,
+  reducers: {
+    setTableInfo(state, { payload }) {
+      return {
+        ...state,
+        id: payload.id,
+        status: payload.status,
+        clients_number: payload.clients_number,
+        products:payload.products
+      };
+    },
+  },
+});
+export const { setTableInfo } = slice.actions;
+export default slice.reducer;
