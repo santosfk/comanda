28efa401c471133d6c730cd8957db339facdeb4d
santosfk <santosguilhermefk@gmail.com>
2023-01-04 22:05:58 -0300

    feat: add new color in colors

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -1,5 +1,7 @@
 export default {
   primary: "#2EDBBC",
   primary_background: "#101010",
+  background_green_100:"#E8F2F0",
   primary_text: "#FFFFFF",
+  yellow:"#E9AB0B"
 };
--- src/theme/light.ts
+++ src/theme/light.ts
@@ -1,5 +1,7 @@
 export default {
   primary: "#2EDBBC",
   primary_background: "#FFFFFF",
+  background_green_100:"#E8F2F0",
   primary_text: "#282121",
+  yellow:"#E9AB0B"
 };
