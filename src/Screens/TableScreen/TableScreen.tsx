2799d55adb80a5f7eed574c7a6e50f182824c078
santosfk <santosguilhermefk@gmail.com>
2023-01-07 17:11:34 -0300

    feat: create table screen component

---
+++ src/Screens/TableScreen/TableScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+export const TableScreen = () => {
+  const { id, status, clients_number, products } = useSelector(
+    (state: any) => state.tableData
+  );
+  console.log(id, status, clients_number, products)
+  return (
+    <View>
+      <Text>TableScreen</Text>
+    </View>
+  );
+};
---
+++ src/Screens/TableScreen/index.ts
@@ -0,0 +1,1 @@
+export {TableScreen} from './TableScreen' 
