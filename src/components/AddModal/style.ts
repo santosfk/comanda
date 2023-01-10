296f11f161e066cc9db3e4908b1d7371a22ebe8b
santosfk <santosguilhermefk@gmail.com>
2023-01-09 23:12:14 -0300

    feat: add style in modal container

--- src/components/AddModal/style.ts
+++ src/components/AddModal/style.ts
@@ -1,7 +1,14 @@
 import styled from "styled-components/native";
 export const Container = styled.View`
 flex: 1;
-background-color: rgba(0,0,0,0.1);
-
+height: 500px;
+width: 300px;
+background-color: rgba(0,0,0,0.5);
+position: absolute;
+z-index: 2;
+display: flex;
+align-items: center;
+justify-content: center;
+border-radius: 20px;
 
 `
