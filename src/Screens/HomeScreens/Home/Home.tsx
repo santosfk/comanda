37ad959fa1ca26ca33003ab4a856a57adce4ded7
santosfk <santosguilhermefk@gmail.com>
2023-01-11 10:06:40 -0300

    feat : modal to add more items

--- src/Screens/HomeScreens/Home/Home.tsx
+++ src/Screens/HomeScreens/Home/Home.tsx
@@ -1,26 +1,29 @@
-import React, { useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { Container } from "./style";
 import table from "../../../mocks/table.json";
 import { Table } from "../../../components/Table";
 import { setTablesData } from "../../../redux/allTableData";
-import { useDispatch, useSelector } from "react-redux";
+import { AddModal } from "../../../components/AddModal";
+import { Button } from "../../../components/Button";
+import { useTablesData } from "../../../utils/getTablesData";
+import tables from '../../../mocks/table.json'
+import { useInitialData } from "../../../utils/setInitialData";
 
 export const Home = () => {
-  const dispatch = useDispatch();
-  const tableData = table;
-  dispatch(setTablesData(tableData));
-  const { data } = useSelector((state: any) => state.tablesData);
-  console.log(data)
+  const [modalOn, setModalOn] = useState(false);
+  useInitialData()
+  const {data} = useTablesData()
   return (
     <Container>
+      {modalOn && <AddModal />}
       <FlatList
         style={{ paddingTop: 50 }}
         data={data}
         ListHeaderComponent={<Text>Mesas</Text>}
         renderItem={({ item }) => <Table key={item.id} data={item} />}
       />
+     <Button fontSize={12} bgColor="#2EDBBC" color="white" onPress={()=>setModalOn(!modalOn)}>Adicionar</Button>
     </Container>
   );
 };
-
