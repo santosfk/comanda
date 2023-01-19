66397105efd5374908129e6ac4522cb3c76b5ab4
santosfk <santosguilhermefk@gmail.com>
2023-01-19 18:35:07 -0300

    feat: add request functions in app

--- App.tsx
+++ App.tsx
@@ -6,14 +6,23 @@ import { InitialRoutes } from "./src/Routes";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
 import theme from "./src/theme";
+import { setNewTable } from "./src/api/api";
 export default function App() {
   const deviceTheme = useColorScheme();
+  const setTable = async () => {
+    await setNewTable({
+      id: 3,
+      clientsNumber: 4,
+      products: [],
+      status: "aprovado",
+    });
+  };
 
   return (
     <Provider store={store}>
-    <ThemeProvider theme={theme.light} >
-      <InitialRoutes />
-    </ThemeProvider>
+      <ThemeProvider theme={theme.light}>
+        <InitialRoutes />
+      </ThemeProvider>
     </Provider>
   );
 }
