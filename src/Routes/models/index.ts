0afdcc5fafc41decfc9d78b3ec8ec5e30674fbdd
santosfk <santosguilhermefk@gmail.com>
2023-01-18 23:29:40 -0300

    feat: adding new route in typing

--- src/Routes/models/index.ts
+++ src/Routes/models/index.ts
@@ -2,9 +2,10 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 type RootParamsList = {
   checkin: undefined;
   home: undefined;
-  homeRoutes:undefined;
-  getName:undefined;
-  search :undefined
-  tableScreen :undefined
+  homeRoutes: undefined;
+  getName: undefined;
+  search: undefined;
+  tableScreen: undefined;
+  addTable: undefined;
 };
 export type propsStack = NativeStackNavigationProp<RootParamsList>;
