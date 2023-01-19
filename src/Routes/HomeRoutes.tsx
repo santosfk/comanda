0efb4e9c103c6debcd47a58019515cd4f8c2a310
santosfk <santosguilhermefk@gmail.com>
2023-01-18 23:29:09 -0300

    feat: add new route in home routes

--- src/Routes/HomeRoutes.tsx
+++ src/Routes/HomeRoutes.tsx
@@ -6,38 +6,39 @@ import { Home } from "../Screens/HomeScreens/Home";
 import { AntDesign, Entypo, FontAwesome, Ionicons } from "@expo/vector-icons";
 import { Search } from "../Screens/HomeScreens/Search";
 import { User } from "../Screens/HomeScreens/User";
+import { AddTable } from "../Screens/HomeScreens/AddTable";
 const Tab = createBottomTabNavigator();
 export const HomeRoutes = () => {
   return (
     <Tab.Navigator
-    screenOptions={({ route }) => ({
-      headerShown:false,
-      tabBarIcon: ({ focused, size }) => {
-        let iconName;
-        let color = focused? "#2EDBBC" : 'black'
-        if (route.name === 'home') { 
-          return <Entypo name="home"  size={size} color={color}/> 
-         }
-         if(route.name === 'search'){
-          return <FontAwesome name="search" size={size} color={color}/>
-         }
-         if(route.name === 'user'){
-          return <FontAwesome name="user" size={size} color={color}/>
-         }
-         
-    
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, size }) => {
+          let iconName;
+          let color = focused ? "#2EDBBC" : "black";
+          if (route.name === "home") {
+            return <Entypo name="home" size={size} color={color} />;
+          }
+          if (route.name === "search") {
+            return <FontAwesome name="search" size={size} color={color} />;
+          }
+          if (route.name === "user") {
+            return <FontAwesome name="user" size={size} color={color} />;
+          }
+          if (route.name === "addTable") {
+            return <AntDesign name="pluscircle" size={24} color={color} />;
+          }
 
-        // You can return any component that you like here!
-       
-      
-      },
-      tabBarActiveTintColor: '#2EDBBC',
-      tabBarInactiveTintColor: 'gray',
-    })}
+          // You can return any component that you like here!
+        },
+        tabBarActiveTintColor: "#2EDBBC",
+        tabBarInactiveTintColor: "gray",
+      })}
     >
-      <Tab.Screen name="home" component={Home}  />
-      <Tab.Screen name="search" component={Search}  />
-      <Tab.Screen name="user" component={User}  />
+      <Tab.Screen name="home" component={Home} />
+      <Tab.Screen name="search" component={Search} />
+      <Tab.Screen name="user" component={User} />
+      <Tab.Screen name="addTable" component={AddTable} />
     </Tab.Navigator>
   );
 };
