0d335773193387a57e950d1ce809a8e0f42ed152
santosfk <santosguilhermefk@gmail.com>
2023-01-18 23:30:47 -0300

    feat: adjusting padding in home route

--- src/Screens/HomeScreens/Home/style.ts
+++ src/Screens/HomeScreens/Home/style.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-flex: 1;
-align-items: center;
-justify-content: center;
-background-color: ${({theme})=> theme.primary_background};
-
-`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.primary_background};
+  padding-bottom: 30px;
+`;
