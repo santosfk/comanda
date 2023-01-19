98b90dda757323856c16fd56f4eda84a5e5d7f43
santosfk <santosguilhermefk@gmail.com>
2023-01-18 23:31:01 -0300

    feat: create addTable component

---
+++ src/Screens/HomeScreens/AddTable/AddTable.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export const AddTable = () => {
+  return (
+    <View>
+      <Text>AddTable</Text>
+    </View>
+  );
+};
