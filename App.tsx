fe38dee7ec2ea0a48919f3be55c85222b230c984
santosfk <santosguilhermefk@gmail.com>
2023-01-20 11:39:09 -0300

    fix: format code

--- App.tsx
+++ App.tsx
@@ -6,17 +6,9 @@ import { InitialRoutes } from "./src/Routes";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
 import theme from "./src/theme";
-import { setNewTable } from "./src/api/api";
+import { DeleteTable, setNewTable } from "./src/api/api";
 export default function App() {
   const deviceTheme = useColorScheme();
-  const setTable = async () => {
-    await setNewTable({
-      id: 3,
-      clientsNumber: 4,
-      products: [],
-      status: "aprovado",
-    });
-  };
 
   return (
     <Provider store={store}>
