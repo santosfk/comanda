f3b862381964a4861feecdbff1e4e0a4abd6b697
santosfk <santosguilhermefk@gmail.com>
2023-01-28 23:23:27 -0300

    feat: import  styles in page

--- src/Screens/HomeScreens/AddTable/AddTable.tsx
+++ src/Screens/HomeScreens/AddTable/AddTable.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
-
+import { Container } from "./style";
 export const AddTable = () => {
   return (
-    <View>
+    <Container>
       <Text>AddTable</Text>
-    </View>
+    </Container>
   );
 };
